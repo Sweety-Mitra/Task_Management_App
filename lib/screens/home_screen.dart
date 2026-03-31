b98c70293547c9b867f1b4b833ba66d0531db21d
Sweety Mitra <118052909+Sweety-Mitra@users.noreply.github.com>
2026-03-31 20:27:00 +0530

    Display task list in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/task_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Task Manager")),
-      body: Center(child: Text("No tasks yet")),
+
+      body: Consumer<TaskProvider>(
+        builder: (context, provider, child) {
+          if (provider.tasks.isEmpty) {
+            return Center(child: Text("No tasks yet"));
+          }
+
+          return ListView.builder(
+            itemCount: provider.tasks.length,
+            itemBuilder: (context, index) {
+              final task = provider.tasks[index];
+
+              return ListTile(
+                title: Text(task.title),
+                subtitle: Text(task.description),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
